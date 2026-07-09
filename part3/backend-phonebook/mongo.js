f8ff6f5c141dae9d9edfd05382c35cd753e2059e
Juan Bautista Malina <malinajuanbautista@gmail.com>
2026-07-08 21:17:01 -0300

    feat(phonebook): simplificar el archivo de pruebas

--- part3/backend-phonebook/mongo.js
+++ part3/backend-phonebook/mongo.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Ejecutar: node mongo.js yourPassword || node mongo.js yourPassword name number
-
-// Obtener los argumentos de la línea de comandos
-const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
-
 // Configurar url de la db y realizar la conexión
-const url = `${process.env.MONGODB_BASE_URL}${password}@cluster0.nz96pis.mongodb.net/phonebookApp?appName=Cluster0`;
+const url = process.env.MONGODB_URI;
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
@@ -20,21 +13,20 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", personSchema);
 
-if (process.argv.length === 3) {
-  // Obtener todas las entradas de la base de datos y mostratrlas por consola
-  Person.find({}).then((result) => {
-    console.log("phonebook:");
-    result.forEach((person) => console.log(`${person.name} ${person.number}`));
-    mongoose.connection.close();
-  });
-} else {
-  
-  // Crear una nueva persona usando el modelo
-  const person = new Person({ name, number });
+// Crear una nueva persona usando el modelo
+// const person = new Person({ name: "Arto Hellas", number: "040-123456" });
+// const person = new Person({name: "Ada Lovelace", number: "39-44-5323523"});
+// const person = new Person({name: "Dan Abramov", number: "12-43-234345"})
+// const person = new Person({name: "Mary Poppendieck", number: "39-23-6423122"})
 
-  // Guardar persona en la base de datos
-  person.save().then((result) => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
-}
+// Guardar persona en la base de datos
+// person.save().then((result) => {
+//   console.log(`added ${result.name} number ${result.number} to phonebook`);
+//   mongoose.connection.close();
+// });
+
+Person.find({}).then((people) => {
+  console.log("Phonebook:")
+  people.map(p => console.log(`${p.name}, ${p.number}`));
+  mongoose.connection.close()
+});
