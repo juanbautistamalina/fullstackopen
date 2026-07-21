784b22ed226cb4574cac1a746affa6caeea009cd
Juan Bautista Malina <malinajuanbautista@gmail.com>
2026-07-21 16:10:06 -0300

    feat(phonebook): agregar eslint y corregir advertencias

---
+++ part3/backend-phonebook/eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "module",
+    },
+  },
+]);
--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -7,7 +7,7 @@ const Person = require("./models/people");
 app.use(express.json());
 app.use(express.static("dist"));
 
-morgan.token("body", function (req, res) {
+morgan.token("body", function (req) {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
   }
